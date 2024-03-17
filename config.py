6c01ff19bbfa929962f8074c26560226514e087e
Moalmortagel <105941198+Almortage@users.noreply.github.com>
2024-03-18 00:36:25 +0200

    Update

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+
+class Config(object):
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    API_ID = os.environ.get("API_ID", "")
+    API_HASH = os.environ.get("API_HASH", "")
+
+    
